960a0fe95d451ca01eef946ef9a163583362f516
Muhammad <muhammadahjahh@gmail.com>
2024-04-25 17:32:43 +0700

    final retro

--- Retrofit-Practice/app/src/main/java/com/dicoding/retrofit_practice/MainActivity.kt
+++ Retrofit-Practice/app/src/main/java/com/dicoding/retrofit_practice/MainActivity.kt
@@ -3,11 +3,13 @@ package com.dicoding.retrofit_practice
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.dicoding.retrofit_practice.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,12 +18,17 @@ class MainActivity : AppCompatActivity() {
     private val baseUrl = "https://jsonplaceholder.typicode.com/"
 
     var postsList = ArrayList<Response>()
+
+    lateinit var adapter: Adapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         showPosts()
+
+        binding.rvRetrofit.layoutManager = LinearLayoutManager(this)
     }
 
     fun showPosts(){
@@ -39,7 +46,13 @@ class MainActivity : AppCompatActivity() {
                 call: Call<List<Response>>,
                 response: retrofit2.Response<List<Response>>
             ) {
-
+                if(response.isSuccessful){
+                    binding.progressBar.isVisible = false
+                    binding.rvRetrofit.isVisible = true
+                    postsList = response.body() as ArrayList<Response>
+                    adapter = Adapter(postsList)
+                    binding.rvRetrofit.adapter = adapter
+                }
             }
             override fun onFailure(call: Call<List<Response>>, t: Throwable) {
                 Toast.makeText(applicationContext,t.localizedMessage,Toast.LENGTH_SHORT).show()
